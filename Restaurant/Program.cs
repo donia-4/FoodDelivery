b91c490f27206c03d4afb157f2f80af3e4c708ff
Donia Shaban <doniashaban723@gmail.com>
2026-07-28 14:05:09 +0300

    feat(api-gateway): bootstrap API Gateway with Ocelot

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddPresentationServices(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
